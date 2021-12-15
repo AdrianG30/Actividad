538218b12bbaf2c699a55a6f9820df1661177356
Renato Gomez <gomezrenato193@gmail.com>
2021-12-14 21:42:57 -0500

    Actividad

--- app/src/main/java/com/renato/actividad/LoginFragment.java
+++ app/src/main/java/com/renato/actividad/LoginFragment.java
@@ -8,12 +8,21 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.button.MaterialButton;
+
 public class LoginFragment extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState
     ){
         View view =inflater.inflate(R.layout.login_fragment,container,false);
+        MaterialButton loginButton=view.findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((NavigationHost) getActivity()).navigateTo(new HomeFragment(),false);
+            }
+        });
         return view;
     }
 }
